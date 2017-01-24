6b47bc0fa4d154e8b8bb9bdf35796e1d927f4c70
Barrett Amsrud <barrettamsrud@gmail.com>
2017-01-23 18:54:56 -0600

    Added params object

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,19 @@
 //  Import PostgreSQL
 const pg = require('pg');
+const url = require('url');
 
 //  Set database
-var config= {
+var config = {
   // database: 'solo'
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true
 };
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
 
 //  Set connection pool
 var pool = new pg.Pool(config);
